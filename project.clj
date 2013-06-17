78acca748e9071d1e2b50e5111abc589ebc85eb2
supersymmetry <maildumpert@gmail.com>
2013-06-17 10:00:01 +0200

    updated deps/plugins versions

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clojure-accumulo/clojure-accumulo "0.1.0-SNAPSHOT"
   :description "Clojure bindings for Apache Accumulo"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.hadoop/hadoop-core "0.20.2"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.thrift/libthrift "0.9.0"]
+                 [org.apache.hadoop/hadoop-core "1.2.0"
                   :exclusions [ant/ant
                                junit/junit
                                org.mortbay.jetty/jetty
@@ -11,12 +12,12 @@
                                org.mortbay.jetty/servlet-api-2.5
                                tomcat/jasper-compiler
                                tomcat/jasper-runtime]]
-                 [org.apache.zookeeper/zookeeper "3.3.3"
+                 [org.apache.zookeeper/zookeeper "3.4.5"
                    :exclusions [com.sun.jmx/jmxri
                                 com.sun.jdmk/jmxtools
                                 javax.jms/jms
                                 jline/jline]]
-                 [org.apache.accumulo/accumulo-core "1.3.6"
+                 [org.apache.accumulo/accumulo-core "1.5.0"
                   :exclusions [jline/jline
                                junit/junit]]
                  ])
